a833c4e0401ab28afa1b2b525cac3ba6d824182c
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-05 21:16:05 +0530

    Sanity Check

---
+++ src/Publisher/Publisher.java
@@ -0,0 +1,14 @@
+package Publisher;
+
+/**
+ * @Author : Pushkarraj Pujari
+ * @Since : 05/09/2017
+ */
+public class Publisher {
+    /**
+     * Sanity Check
+     * */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
